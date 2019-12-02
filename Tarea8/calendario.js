8f826109888d09ad7b1f958ef3119a815ce8555e
Jomsz <erick.jomsz@gmail.com>
2019-12-02 02:27:54 -0400

    Se subio la tarea 8

---
+++ Tarea8/calendario.js
@@ -0,0 +1,58 @@
+function cambiarMes(opcion){
+	var d = new Date();
+    var mes = d.getMonth() + 1;
+    var anio = d.getFullYear();
+	switch(opcion){
+		case "-anio":
+		anio--;
+		break;
+		case "-mes":
+			if(mes == 1){
+				anio --;
+				mes = 12;
+			}
+			else
+				mes--;
+		break;
+		case "+mes":
+			if(mes == 12){
+				anio ++;
+				mes = 1;
+			}
+			else
+				mes++;
+		break;
+		case "+anio":
+		anio++
+		break;
+	}
+	llamarGET('ajax.php',mes,anio);
+}
+
+function llamarGET(direccion,mes,anio){
+	direccion += '?mes=' + mes + '&anio=' + anio;
+	ajax=new XMLHttpRequest();
+	ajax.onreadystatechange=function(){
+		if(ajax.readyState == 4) { 
+		 	if(ajax.status == 200){
+		 	 document.getElementById('contenido').innerHTML=ajax.responseText;
+		 	}
+		}
+	}
+	ajax.open('GET', direccion, true); 
+	ajax.send(); 
+}
+
+function llamarPOST(direccion,mes,anio){
+	ajax=new XMLHttpRequest();
+	ajax.onreadystatechange=function(){
+		if(ajax.readyState == 4) { 
+			if(ajax.status == 200) { 
+			 	 document.getElementById('contenido').innerHTML=ajax.responseText;
+			}
+		}	
+	}
+	ajax.open('POST', direccion, true);
+	ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); 
+	ajax.send(mes,anio); 
+}
